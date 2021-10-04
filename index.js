cffb3d64fc00b6950cac262eec9cdd10df93e65f
Abhishek Muralidharan <75883645+abhishek68@users.noreply.github.com>
2021-10-04 12:16:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var express = require('express')
 var app = express()
 const PORT =process.env.PORT || 3000
-app.get('/home/:id',function(req,res){
-       res.send(req.params.id)
-})
-app.get('/home', function (req, res) {
+
+app.get('/', function (req, res) {
   res.send('hello world')
 })
 app.listen(PORT,()=>{
