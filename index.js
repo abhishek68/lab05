6480a782a23de2b4367ad6af337198266b3addc0
Abhishek Muralidharan <75883645+abhishek68@users.noreply.github.com>
2021-10-04 12:54:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express')
 var app = express()
 const PORT =process.env.PORT || 3000
-
 app.get('/', function (req, res) {
-  res.send('hello world')
+  res.sendFile( __dirname + "/" + "index.html" );
+})
+app.get('/meat', function (req, res) {
+  res.sendFile( __dirname + "/" + "meat.html" );
 })
 app.listen(PORT,()=>{
              console.log("Listening on port:"+PORT)
